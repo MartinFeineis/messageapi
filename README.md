ecc1e9e1119fcec5cb8f4e67dac481bdcf2dba6d
Martin Feineis <martifein@gmail.com>
2020-12-28 23:02:39 -0500

    setting up .env File

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ A quick message api with django restframework
 Python Version  Python 3.9.0
 Pip Version  pip 20.2.3 from /home/wolle/.pyenv/versions/3.9.0/lib/python3.9/site-packages/pip (python 3.9)
 Django Version 3.1.4
-
+##Setup Dev-Env
+```
+pyenv local 3.9.0
+python -m venv .
+```
 dev superuser wolle admin
 curl example for GET first example retrieves List of all Posts second example retrieves the POST with id 3
 ```
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,14 +74,34 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresdb',
+        'USER': 'postgresadmin',
+        'PASSWORD': 'admin123',
+        'HOST': '192.168.99.101',
+        'PORT': '31202', 
+    }                                                                           
 }
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'), 
+    }                                                                           
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
