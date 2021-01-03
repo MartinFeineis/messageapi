50f43bf9ed16734059e5a3fed9d2baac4f20a3b8
Martin Feineis <martifein@gmail.com>
2021-01-03 14:37:33 -0500

    adding py-pip3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apk update
 #apk add --update
-apk add --no-cache python3 zip python3-dev gcc make postgresql-dev musl-dev libxml2-dev libxslt-dev g++ gcc-avr linux-headers curl libcurl
+apk add --no-cache python3 zip python3-dev py3-pip gcc make postgresql-dev musl-dev libxml2-dev libxslt-dev g++ gcc-avr linux-headers curl libcurl
 pip3 install --upgrade pip
 pip3 install cython
 pip3 install --no-build-isolation --no-cache-dir -r requirements.txt
